2ab3b7712c9a04805afbc7d45c3651d5a497ea51
Miroslav Pilat <miroslav.pilat@gmail.com>
2021-03-19 18:15:05 +0100

    A working version of the api.

--- app.py
+++ app.py
@@ -3,13 +3,35 @@ app = Flask(__name__)
 
 app.config.from_object('config.DevConfig')
 
-lights = { 
-    "bouncy_pixels_service" : "Lights are flowing through the HAT and bouncing from it's margins.",
-    "matrix_service" : "Lights are flowing through the HAT in Matrix fasion.",
-    "rainbow_service" : "Lights are flowing through the HAT in rainbow patterns." 
-    }
+lights = [ 
+    {"id": 1, "state": 0, "name": "bouncy_pixel_service.service", "description": "Lights are flowing through the HAT and bouncing from it's margins."},
+    {"id": 2, "state": 0, "name": "matrix_service.service", "description": "Lights are flowing through the HAT in Matrix fasion."},
+    {"id": 3, "state": 0, "name": "rainbow_service.service", "description": "Lights are flowing through the HAT in rainbow patterns."} 
+]
 
-#Routes
+## Systemctl
+from pydbus import SystemBus
+
+bus = SystemBus()
+
+systemd = bus.get(".systemd1")
+#systemd = bus.get("org.freedesktop.systemd1")
+
+manager = systemd[".Manager"]
+#manager = systemd["org.freedesktop.systemd1.Manager"]
+#manager = systemd # works but may break if systemd adds another interface
+
+import sys
+
+def systemctl(cmd, *name):
+    try:
+        command = cmd
+        command = "".join(x.capitalize() for x in command.split("-"))
+        result = getattr(manager, command)(*name)
+    except Exception as e:
+        print(e)
+
+## Routes
 
 @app.route("/")
 @app.route("/api")
@@ -18,16 +40,43 @@ def hello():
     return jsonify(response)
 
 @app.route("/api/lights")
-def list_lights():
+@app.route("/api/lights/all")
+def get_lights():
     return jsonify(lights)
+
+@app.route("/api/lights/on/<int:light_id>", methods=['GET'])
+def set_light_on(light_id):
+    # Check if other light is ON and switch it off first
+    for l in lights:
+        if l["state"] == 1:
+            set_light_off(l["id"])
+    # Switch ON the light we want
+    systemctl("start-unit", lights[light_id-1]["name"], "replace")
+    lights[light_id-1]["state"] = 1
+    response = {"message":"Light " + lights[light_id-1]["name"] + " on."}
+    return jsonify(response)
+
+@app.route("/api/lights/off/<int:light_id>", methods=['GET'])
+def set_light_off(light_id):
+    lights[light_id-1]["state"] = 0
+    systemctl("stop-unit", lights[light_id-1]["name"], "replace")
+    response = {"message":"Light " + lights[light_id-1]["name"] + " off."}
+    return jsonify(response)
+
+@app.route("/api/lights/off")
+def set_lights_off():
+    for l in lights:
+        set_light_off(l["id"])
+    response = {"message":"Lights turned off."}
+    return jsonify(response)
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': '404 - Issue Not found'}), 404)
+    return make_response(jsonify({'error': '404 - Not found'}), 404)
 
 @app.errorhandler(503)
 def not_found(error):
-    return make_response(jsonify({'error': '503 - There was an error calling Jira.'}), 503)
+    return make_response(jsonify({'error': '503 - There was an error.'}), 503)
 
 
 if __name__ == '__main__':
